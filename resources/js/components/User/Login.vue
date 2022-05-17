96c26139aed20e77578e0260f944f1bb0a3e5302
Roman <roma20.94.a@gmail.com>
2022-05-17 08:11:07 +0300

    add registration

---
+++ resources/js/components/User/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-25 mt-3">
+        <input type="email" class="form-control mb-2" placeholder="Email">
+        <input type="password" class="form-control mb-2" placeholder="Password">
+        <input type="submit" class="btn btn-primary">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/User/Registration.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="w-25 mt-3">
+            <input v-model="name" type="text" class="form-control mb-2" placeholder="Name">
+            <input v-model="email" type="email" class="form-control mb-2" placeholder="Email">
+            <input v-model="password" type="password" class="form-control mb-2" placeholder="Password">
+            <input v-model="password_confirmation" type="password" class="form-control mb-2" placeholder="Confirm password">
+            <input @click.prevent="store" type="submit" class="btn btn-primary">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Registration",
+
+    data() {
+        return {
+            name: null,
+            email: null,
+            password: null,
+            password_confirmation: null,
+        }
+    },
+
+    methods: {
+        store() {
+            axios.post('/api/users', {name: this.name, email: this.email, password: this.password, password_confirmation: this.password_confirmation})
+            .then(res => {
+                console.log(res);
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
